ed7266a9932b7bdc8cff9c4429c5b4c50f316c64
Shrutikumari05 <shrutimishra493@gmail.com>
2023-10-13 04:36:16 +0530

    updated

--- uawwebsite/src/component/pages/home/Home.js
+++ uawwebsite/src/component/pages/home/Home.js
@@ -12,117 +12,139 @@ import img4 from "../../assets/img4.jpg";
 
 function Home() {
   return (
-    // <>
-    <div className="container home">
-      <div className="row gx-0 d-flex homeContent">
-        <div className="col-6 d-grid">
-          <span className="textDiv d-block">Engineering</span>
-          <span className="textDiv">Redefined</span>
-          <h6 className="homeSubSec">
-            Seasoned Technopreneurs, Pioneering Software Solutions{" "}
-          </h6>
-        </div>
-        <div class8remName="col-6 imgSection d-grid">
-          {/* <img src={unused1} class="rounded heroImg" alt="" /> */}
-          {/* <img src={unused2} class="rounded heroImg" alt="" /> */}
+    // <div>
+    <div className="container-fluid home">
+      <div className="container">
+        <div className="row gx-0 d-flex homeContent">
+          <div className="col-6 d-grid">
+            <span className="textDiv d-block">Engineering</span>
+            <span className="textDiv">Redefined</span>
+            <h6 className="homeSubSec">
+              Seasoned Technopreneurs, Pioneering Software Solutions{" "}
+            </h6>
+          </div>
+          <div class8remName="col-6 imgSection d-grid">
+            {/* <img src={unused1} class="rounded heroImg" alt="" /> */}
+            {/* <img src={unused2} class="rounded heroImg" alt="" /> */}
+          </div>
         </div>
       </div>
+
       {/* MiddleSection */}
 
-      <div className="row gx-0 middleSection">
-        {/* <div className="col-12 d-flex"> */}
-        <h1 className="textDiv homeText d-flex justify-content-center">
-          Our Services
-        </h1>
+      <div
+        className="container-fluid"
+        style={{ backgroundColor: " rgb(219, 219, 219)" }}
+      >
+        <div className="row gx-0 middleSection">
+          {/* <div className="col-12 d-flex"> */}
+          <h1
+            className=" heading textDiv homeText d-flex justify-content-center"
+            style={{ fontSize: "100" }}
+          >
+            Our Services
+          </h1>
 
-        <div className="row gx-0 midDiv">
-          <div className="midImg col-6 mt-5">
-            <img src={img15} class="service_img" alt="" />
+          <div className="row gx-0 midDiv">
+            <div className="midImg col-6 mt-5">
+              <img src={img15} class="service_img" alt="" />
+            </div>
+            <div
+              className="col-6"
+              style={{ top: "180px", position: "relative" }}
+            >
+              <h4 className="textDiv">Semiconductor</h4>
+              <h6>
+                Our top-notch, tailor-made softwareproducts simplify
+                collaboration onintricate tasks, enhancing efficiency
+                andingenuity.
+              </h6>
+            </div>
+            {/* </div> */}
           </div>
-          <div className="col-6" style={{ top: "180px", position: "relative" }}>
-            <h4 className="textDiv">Semiconductor</h4>
-            <h6>
-              Our top-notch, tailor-made softwareproducts simplify collaboration
-              onintricate tasks, enhancing efficiency andingenuity.
-            </h6>
-          </div>
-          {/* </div> */}
-        </div>
 
-        <div className="row gx-0 midDiv slideInRight">
-          <div className="col-6" style={{ top: "180px", position: "relative" }}>
-            <h4 className="textDiv">Telecom</h4>
-            <h6>
-              Our top-notch, tailor-made softwareproducts simplify collaboration
-              onintricate tasks, enhancing efficiency andingenuity.
-            </h6>
-          </div>
-          <div className="col-6 midImg tele">
-            <img src={img13} class="service_img" alt="" />
-          </div>
-        </div>
-
-        <div className="row gx-0 midDiv">
-          <div className="midImg col-6 mt-4">
-            <img src={img14} class="service_img" alt="" />
-          </div>
-          <div className="col-6" style={{ top: "180px", position: "relative" }}>
-            <h4 className="textDiv">Automotive</h4>
-            <h6>
-              Our top-notch, tailor-made softwareproducts simplify collaboration
-              onintricate tasks, enhancing efficiency andingenuity.
-            </h6>
-          </div>
-        </div>
-      </div>
-      {/* </div> */}
-
-      <h1 className="textDiv homeText lastDiv d-flex  mt-5">Our Partners</h1>
-
-      <div class="row align-item-center d-flex mt-5">
-        <div className="col-4 myCard rounded">
-          <div class="card cardView">
-            <img src={img4} class="card-img-top rounded" alt="" />
-            <div class="card-body">
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+          <div className="row gx-0 midDiv slideInRight">
+            <div
+              className="col-6"
+              style={{ top: "180px", position: "relative" }}
+            >
+              <h4 className="textDiv">Telecom</h4>
+              <h6>
+                Our top-notch, tailor-made softwareproducts simplify
+                collaboration onintricate tasks, enhancing efficiency
+                andingenuity.
+              </h6>
+            </div>
+            <div className="col-6 midImg tele">
+              <img src={img13} class="service_img" alt="" />
             </div>
           </div>
-        </div>
 
-        <div className="col-4">
-          <div class="card cardView">
-            <img src={img4} class="card-img-top rounded" alt="" />
-            <div class="card-body">
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+          <div className="row gx-0 midDiv">
+            <div className="midImg col-6 mt-4">
+              <img src={img14} class="service_img" alt="" />
             </div>
-          </div>
-        </div>
-        <div className="col-4">
-          <div class="card cardView">
-            <img src={img4} class="card-img-top rounded" alt="" />
-            <div class="card-body">
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+            <div
+              className="col-6"
+              style={{ top: "180px", position: "relative" }}
+            >
+              <h4 className="textDiv">Automotive</h4>
+              <h6>
+                Our top-notch, tailor-made softwareproducts simplify
+                collaboration onintricate tasks, enhancing efficiency
+                andingenuity.
+              </h6>
             </div>
           </div>
         </div>
       </div>
-      {/* <div className="animationSection"> */}
 
-      {/* </div> */}
+      <div className="container">
+        <div class="row align-item-center d-flex mt-5 testimonials">
+          <h1 className="textDiv homeText lastDiv d-flex  mt-5 justify-content-center">
+            Our Partners
+          </h1>
+
+          <div className="col-4 myCard rounded px-5 mt-5">
+            <div class="card_main ">
+              <img src={img4} class="card-img-top rounded" alt="" />
+              <div class="card-body">
+                <p class="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-4 myCard  px-5 mt-5">
+            <div class="card_main ">
+              <img src={img4} class="card-img-top rounded" alt="" />
+              <div class="card-body">
+                <p class="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 myCard px-5 mt-5">
+            <div class="card_main">
+              <img src={img4} class="card-img-top rounded" alt="" />
+              <div class="card-body">
+                <p class="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    //{" "}
   );
 }
 
